3c48241398749a44f20af3c1d24dff42df27776b
YagoLisboa <yago.lisboa@live.com>
2024-02-25 17:33:37 -0300

    Configurando e Trabalhando com tipos de Variáveis em TypeScript

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+/**
+ * variáveis
+ */
+
+//tipos primitivos: boolean, number, string
+    let ligado: boolean = false;
+    let nome: string = "Yago";
+    let idade: number = 28;
+    let altura: number = 1.62;
+
+//tipos especiais: null, undefined
+    let nulo: null = null;
+    let indefinido: undefined = undefined;
+
+//tipos abrangentes: any, void
+    let retorno: void
+    let retornoView: any = "Qualquer retorno não previsível"
+
